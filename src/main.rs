3817ea3431c539db62e0ac154d7cd81e8ece8259
Sophie <87262182+iSophes@users.noreply.github.com>
2026-08-21 07:34:50 +0100

    :sparkles: feat: Add window using gpui and gpui components
    Closes:

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,89 @@
+use gpui::*;
+use gpui_component::{button::*, *};
+
+pub struct PlotDeck;
+
+impl Render for PlotDeck {
+    fn render(&mut self, _: &mut Window, _: &mut Context<Self>) -> impl IntoElement {
+        div()
+            .child(
+                TitleBar::new()
+                    .child(
+                        div()
+                            .flex()
+                            .items_center()
+                            .gap_2()
+                            .child("Home")
+                            .child(IconName::ChevronRight)
+                            .child("Documents")
+                            .child(IconName::ChevronRight)
+                            .child("Project"),
+                    )
+                    .child(
+                        div()
+                            .flex()
+                            .items_center()
+                            .gap_1()
+                            .child(Button::new("search").icon(IconName::Search).ghost()),
+                    ),
+            )
+            .child(
+                div()
+                    .v_flex()
+                    .gap_2()
+                    .flex_1()
+                    .items_center()
+                    .justify_center()
+                    .child("Hello, World!")
+                    .child(
+                        Button::new("ok")
+                            .primary()
+                            .label("Let's Go!")
+                            .on_click(|_, _, _| println!("Clicked!")),
+                    ),
+            )
+    }
+}
+
+// credits to zed for this code
+
+pub fn build_window_options() -> WindowOptions {
+    WindowOptions {
+        titlebar: Some(TitlebarOptions {
+            title: None,
+            appears_transparent: true,
+            traffic_light_position: Some(point(px(9.0), px(9.0))),
+        }),
+        window_bounds: None,
+        focus: false,
+        show: true,
+        kind: WindowKind::Normal,
+        is_movable: true,
+        app_owns_titlebar_drag: true,
+        window_decorations: Some(WindowDecorations::Client),
+        window_min_size: Some(gpui::Size {
+            width: px(360.0),
+            height: px(240.0),
+        }),
+        ..Default::default()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let app = gpui_platform::application().with_assets(gpui_component_assets::Assets);
+
+    app.run(move |cx| {
+        // This must be called before using any GPUI Component features.
+        gpui_component::init(cx);
+
+        cx.spawn(async move |cx| {
+            cx.open_window(build_window_options(), |window, cx| {
+                let view = cx.new(|_| PlotDeck);
+                // This first level on the window, should be a Root.
+                cx.new(|cx| Root::new(view, window, cx))
+            })
+            .expect("Failed to open window");
+        })
+        .detach();
+    });
 }
